6b9d57ad29d554ea0f056a5610d724bc2948e993
jado <tony76.200@hotmail.fr>
2018-05-11 09:47:54 +0200

    save

--- index.php
+++ index.php
@@ -6,5 +6,9 @@ spl_autoload_register('classAutoLoader');
 $toto = new Controler();
 
 $machin = explode("/", $toto->getURL());
+$resultURL = array_shift($machin);
 
+
+echo "<pre>";
 var_dump($machin);
+echo "</pre>";
